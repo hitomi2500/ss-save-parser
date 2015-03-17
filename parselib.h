e573be3484c11cbe57bc026f5acfb9187d076221
hitomi2500 <hitomi2500@mail.ru>
2015-03-17 14:16:23 +0300

    Create parselib.h

---
+++ parselib.h
@@ -0,0 +1,15 @@
+#ifndef PARSELIB_H
+#define PARSELIB_H
+
+#include "mainwindow.h"
+
+enum ParseResult {ParseOk,NotAHeader,DataTooShort};
+
+QDateTime GetDateTimeFromRaw4Byte(QByteArray buf);
+QByteArray GetRaw4ByteFromDateTime(QDateTime dt);
+
+ParseResult ParseHeader(QByteArray buf, SaveType *save);
+ParseResult ParseSAT(QByteArray buf, SaveType *save, int ClusterSize);
+
+#endif // PARSELIB_H
+
